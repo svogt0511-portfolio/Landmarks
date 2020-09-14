9e807068d6ac4f6d3ee7229970fbf65bc0b5130d
Suzanne Vogt <svogt@sumarc.net>
2020-09-14 17:19:57 -0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Suzanne Vogt on 9/14/20.
+//  Copyright © 2020 Suzanne Vogt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
