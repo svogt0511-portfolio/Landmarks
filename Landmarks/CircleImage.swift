a9f174cec41c9c68d613a279eadf542e3decb87a
Suzanne Vogt <svogt@sumarc.net>
2020-09-14 20:43:54 -0400

    SwiftUI Essentials Creating and Combining Views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage..swift
+//  Landmarks
+//
+//  Created by Suzanne Vogt on 9/14/20.
+//  Copyright © 2020 Suzanne Vogt. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
